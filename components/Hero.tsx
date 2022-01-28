a57afcf3bfb2dfc3cf60eb4b9816fbc2637e919e
Brian Chung <brianchung@live.com>
2022-01-27 19:31:59 -0600

    starts hero

---
+++ components/Hero.tsx
@@ -0,0 +1,11 @@
+const Hero = () => {
+  return (
+    <section className="flex flex-col justify-center items-start min-h-screen">
+      <h2 className="text-xl">Hi! My name is</h2>
+      <h1 className="text-5xl">Brian.</h1>
+      <h2 className="text-xl">I build things for the web.</h2>
+    </section>
+  );
+};
+
+export default Hero;
