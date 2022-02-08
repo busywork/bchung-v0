74c80701dd8fb2645df6f06eb8088ea81b83f5f7
Brian Chung <brianchung@live.com>
2022-02-08 15:27:31 -0600

    adds gradient

---
+++ components/Gradient.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+type GradientProps = {
+  children: React.ReactNode;
+  className: string;
+} & React.ComponentPropsWithoutRef<'span'>;
+
+const Gradient = ({ children, className }: GradientProps) => {
+  return (
+    <span
+      className={`${className} font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 dark:from-pink-300 dark:via-purple-300 dark:to-indigo-400`}
+    >
+      {children}
+    </span>
+  );
+};
+
+export default Gradient;
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,8 +1,10 @@
+import Gradient from '../components/Gradient';
+
 const Hero = () => {
   return (
     <section className="flex flex-col justify-center items-start min-h-screen">
       <h2 className="text-xl">Hi! My name is</h2>
-      <h1 className="text-5xl">Brian.</h1>
+      <Gradient className="text-5xl">Brian.</Gradient>
       <h2 className="text-xl">I build things for the web.</h2>
     </section>
   );
