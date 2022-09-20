6a3290d40a11861076d22c304c793e008ef1f50a
Brian Chung <brianchung@live.com>
2022-09-19 19:19:22 -0500

    enables contact button

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,6 @@
 import CloudinaryImage from './CloudinaryImage';
 import Gradient from './Gradient';
+import UnstyledLink from './UnstyledLink';
 
 const Hero = () => {
   return (
@@ -19,12 +20,14 @@ const Hero = () => {
             <span className="relative px-4 py-2 transition-all ease-in duration-150 bg-white dark:bg-black rounded group-hover:bg-opacity-0">
               Résumé
             </span>
-          </button>
-          <button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-black rounded group bg-gradient-to-bl from-green-300 via-blue-500 to-purple-600 group-hover:from-green-300 group-hover:via-blue-600 group-hover:to-purple-600 hover:text-white dark:from-pink-300 dark:via-purple-300 dark:to-indigo-400 dark:text-white focus:outline-none">
-            <span className="relative px-4 py-2 transition-all ease-in duration-150 bg-white dark:bg-black rounded group-hover:bg-opacity-0">
-              Say Hello!
-            </span>
           </button> */}
+          <UnstyledLink href={'mailto:bchung.dev@gmail.com'} openNew={true}>
+            <button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-black rounded group bg-gradient-to-bl from-green-300 via-blue-500 to-purple-600 group-hover:from-green-300 group-hover:via-blue-600 group-hover:to-purple-600 hover:text-white dark:from-pink-300 dark:via-purple-300 dark:to-indigo-400 dark:text-white focus:outline-none">
+              <span className="relative px-4 py-2 transition-all ease-in duration-150 bg-white dark:bg-black rounded group-hover:bg-opacity-0">
+                Say Hello!
+              </span>
+            </button>
+          </UnstyledLink>
         </div>
       </div>
     </section>
